14e4ee1f232b1aadc9608c1ee821f0b5481298fe
Rubens Rodrigues <rubens17rodrigues@gmail.com>
2020-12-13 20:03:15 -0300

    :sparkles: Adiciona página inical ao projeto.

---
+++ learning_logs/urls.py
@@ -0,0 +1,12 @@
+"""Define padrões de URL para learning_logs."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    # Página inicial
+    path('', views.index, name='index'),
+]
