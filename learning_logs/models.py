8a2f55a08fe0d77c0d6865432761b75ca6157d31
Rubens Rodrigues <rubens17rodrigues@gmail.com>
2020-12-13 10:47:12 -0300

    :sparkles: Adiciona o modelo Topic na aplicação.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Topic(models.Model):
+    """Um assunto de interesse do usuário."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma representação em string do modelo."""
+        return self.text
