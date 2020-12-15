7fc14a71d35ad8a0c51694e70bf8c43570541f19
Rubens Rodrigues <rubens17rodrigues@gmail.com>
2020-12-15 16:33:58 -0300

    :art: Adiciona novas páginas na aplicação.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,9 +4,25 @@ from django.urls import path
 
 from . import views
 
+
 app_name = 'learning_logs'
 
 urlpatterns = [
     # Página inicial
     path('', views.index, name='index'),
+
+    # Mostra todos os assuntos
+    path('topics/', views.topics, name='topics'),
+
+    # Págida de detalhes para um único assunto.
+    path('topics/<int:topic_id>/',  views.topic, name='topic'),
+
+    # Página para adicionar um novo assunto.
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    # Página para adicionar uma nova entrada.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # Página para editar uma entrada.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
     """A página inicial de Learning Log."""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco
+        form = TopicForm()
+    else:
+        # Dados de POST submetidos; processa os dados
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Acrescenta uma nova entrda pra um determinado tópico."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco
+        form = EntryForm()
+    else:
+        # Dados de POST submetidos; processa os dados
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edita uma entrada existente."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Requisição inicial; preenche previamente o formulário com a entrada autal
+        form = EntryForm(instance=entry)
+    else:
+        # Dados de POST submetidos; processa os dados 
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
